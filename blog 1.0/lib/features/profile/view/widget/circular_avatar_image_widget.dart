a5082acb065d2c5cce9a62fc843e1f378ee7be01
Hati0kooHati <dserion.prn@gmail.com>
2025-07-20 11:52:14 +0600

    Update circular_avatar_image_widget.dart

--- blog 1.0/lib/features/profile/view/widget/circular_avatar_image_widget.dart
+++ blog 1.0/lib/features/profile/view/widget/circular_avatar_image_widget.dart
@@ -1,25 +1,17 @@
 import 'dart:io';
-
-import 'package:blog/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class CircularAvatarImageWidget extends StatelessWidget {
   const CircularAvatarImageWidget({super.key, required this.imagePath});
 
-  final String? imagePath;
+  final String imagePath;
 
   @override
   Widget build(BuildContext context) {
-    return imagePath != ""
-        ? CircleAvatar(
-            backgroundImage: FileImage(File(imagePath!)),
-            maxRadius: 100,
-            minRadius: 100,
-          )
-        : Container(
-            height: 200,
-            width: 200,
-            decoration: AppTheme.imageFrameDecoration,
-          );
+    return CircleAvatar(
+      backgroundImage: imagePath != "" ? FileImage(File(imagePath)) : null,
+      maxRadius: 100,
+      minRadius: 100,
+    );
   }
 }
