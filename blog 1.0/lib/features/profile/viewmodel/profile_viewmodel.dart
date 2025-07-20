71e067922a4772abdde5432a7df2e2ff3edb4c74
Hati0kooHati <dserion.prn@gmail.com>
2025-07-20 11:45:43 +0600

    Update profile_viewmodel.dart

--- blog 1.0/lib/features/profile/viewmodel/profile_viewmodel.dart
+++ blog 1.0/lib/features/profile/viewmodel/profile_viewmodel.dart
@@ -1,19 +1,22 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:blog/core/service/image_service.dart';
 import 'package:blog/core/utils/utils.dart';
 import 'package:blog/features/profile/model/profile_model.dart';
 import 'package:blog/features/profile/repository/local_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:image_picker/image_picker.dart';
 
 class ProfileViewmodel extends AsyncNotifier<ProfileModel> {
-  late final LocalRepository _localRepository;
-  final ImagePicker _imagePicker = ImagePicker();
+  final LocalRepository _localRepository;
+  final ImageService _imageService;
+
+  File? pickedImage;
+
+  ProfileViewmodel(this._localRepository, this._imageService);
 
   @override
   FutureOr<ProfileModel> build() async {
-    _localRepository = ref.watch(localRepository);
     final data = await _localRepository.profileData;
 
     return mapToProfileModel(data[0]);
@@ -25,7 +28,7 @@ class ProfileViewmodel extends AsyncNotifier<ProfileModel> {
     String? newImagePath;
 
     if (image != null) {
-      newImagePath = await copyImageToAppDocDir(image);
+      newImagePath = await _imageService.copyImageToAppDocDir(image);
     }
 
     final Map<String, String?> newProfileData = {
@@ -41,16 +44,12 @@ class ProfileViewmodel extends AsyncNotifier<ProfileModel> {
   }
 
   Future<File?> pickImage() async {
-    final pickedImage = await _imagePicker.pickImage(
-      source: ImageSource.camera,
-    );
-
-    if (pickedImage == null) {
-      return null;
-    }
-
-    return File(pickedImage.path);
+    return await _imageService.pickImage();
   }
 }
 
-final profileViewmodel = AsyncNotifierProvider(ProfileViewmodel.new);
+final profileViewmodel = AsyncNotifierProvider(
+  () => ProfileViewmodel(LocalRepository(), ImageService()),
+);
+
+final pickedImageProvider = StateProvider<File?>((ref) => null);
