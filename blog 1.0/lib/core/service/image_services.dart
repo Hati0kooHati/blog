45df468355bd2ca826ebe0e1a83c4da4fb10f33e
Hati0kooHati <dserion.prn@gmail.com>
2025-07-20 11:42:44 +0600

    Update image_services.dart

--- blog 1.0/lib/core/service/image_services.dart
+++ blog 1.0/lib/core/service/image_services.dart
@@ -1,1 +1,30 @@
 
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:image_picker/image_picker.dart';
+
+class ImageService {
+  final ImagePicker _imagePicker = ImagePicker();
+
+  Future<String> copyImageToAppDocDir(File image) async {
+    final appDocDir = await syspath.getApplicationDocumentsDirectory();
+    final File copiedImage = await image.copy(
+      "${appDocDir.path}/${path.basename(image.path)}",
+    );
+
+    return copiedImage.path;
+  }
+
+  Future<File?> pickImage() async {
+    final pickedImage = await _imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedImage == null) {
+      return null;
+    }
+
+    return File(pickedImage.path);
+  }
+}
